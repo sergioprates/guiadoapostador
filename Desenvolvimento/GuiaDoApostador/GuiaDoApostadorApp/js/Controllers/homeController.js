96eeaeaad6efdc061fdfff47edca4aa0ec8d1e38
sergioprates <sergioprates.student@gmail.com>
2015-05-23 11:50:16 -0300

    Configurado para compilar diretamente do GitHub

--- Desenvolvimento/GuiaDoApostador/GuiaDoApostadorApp/js/Controllers/homeController.js
+++ Desenvolvimento/GuiaDoApostador/GuiaDoApostadorApp/js/Controllers/homeController.js
@@ -1,13 +1,22 @@
-﻿guiaDoApostador.controller('homeController', ['$scope', function ($scope) {
-    //$('[page=true]').load('paginas/minhasApostas.html', function () { });
+﻿var _db;
+guiaDoApostador.controller('homeController', ['$scope', '$cordovaSQLite', function ($scope, $cordovaSQLite) {
+
+    $scope.executaDB = function () {
+        var query = "INSERT INTO test_table (data, data_num) VALUES (?,?)";
+
+        _db = JSON.parse(window.localStorage.getItem('db'));
+
+       
+    };
 
     $scope.items = [
     {
         id: 1, concurso: '1234',
         loteria: 'Mega-Sena',
         image: 'img/megasena.png',
-        premio: 'R$ 25 milhões', 
-        proximoSorteio: 'dois dias'}];
-} ]);
+        premio: 'R$ 25 milhões',
+        proximoSorteio: 'dois dias'
+    }];
+}]);
 
 
--- Desenvolvimento/GuiaDoApostador/GuiaDoApostadorApp/js/index.js
+++ Desenvolvimento/GuiaDoApostador/GuiaDoApostadorApp/js/index.js
@@ -1,14 +1,50 @@
 ﻿document.addEventListener("deviceready", onDeviceReady, false);
 
 
-var guiaDoApostador = angular.module('ionicApp', ['ionic']);
+//variavel da base de dados
+var _db = null;
+var guiaDoApostador = angular.module('ionicApp', ['ionic', 'ngCordova'])
+    .run(function ($ionicPlatform, $cordovaSQLite) {
+    $ionicPlatform.ready(function () {
+        //if (window.cordova && window.cordova.plugins.Keyboard) {
+         //   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+       // }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+
+        if (window.cordova) {
+            alert('Abrindo o banco de dados');
+            _db = $cordovaSQLite.openDB("my.db"); //device
+
+        } else {
+            _db = window.openDatabase("my.db", '1', 'my', 1024 * 1024 * 100); // browser
+        }
+        $cordovaSQLite.execute(_db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
+
+
+        var query = "INSERT INTO people (firstname, lastname) VALUES (?,?)";
+        $cordovaSQLite.execute(_db, query, ['sergio', 'teste']).then(function (res) {
+            console.log("INSERT ID -> " + res.insertId);
+            alert('idInserido: ' + res.insertId);
+        }, function (err) {
+            alert('Erro na hora de inserir: ' + JSON.stringify(err));
+            console.error(err);
+        });
+
+        window.localStorage.setItem('db', JSON.stringify(_db));
+
+        //$cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
+
+     });
+});
 
 guiaDoApostador.controller('minhasApostasController', ['$scope', function ($scope) {
     //$('[page=true]').load('paginas/minhasApostas.html', function () { });
 }]);
 
 function onDeviceReady() {
-
+    
 
 }
 
