2bad39294ac3f6b188ce7814377e6c1dddcce2f5
claudiosajr <claudio.sa.jr@gmail.com>
2015-06-07 19:31:38 -0300

    Adicionada batch ObtemDadosAtuais com log

---
+++ Desenvolvimento/GuiaDoApostador/GuiaDoApostador_ObtemDadosAtuais/Program.cs
@@ -0,0 +1,22 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObtemDadosAtuais
+{
+    public class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+
+            log.Info("Teste log");
+        }
+    }
+}
