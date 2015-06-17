3394772b3cf26da669e9b1df7c8f8b544038d528
sergioprates <sergioprates.student@gmail.com>
2015-06-16 23:24:41 -0300

    Implementado o Push.

--- Desenvolvimento/GuiaDoApostador/GuiaDoApostadorWebApp/Controllers/ConcursoController.cs
+++ Desenvolvimento/GuiaDoApostador/GuiaDoApostadorWebApp/Controllers/ConcursoController.cs
@@ -1,7 +1,13 @@
 ﻿using GuiaDoApostadorDominio.Entities;
 using GuiaDoApostadorDominio.Interfaces;
+using GuiaDoApostadorWebApp.ViewModels;
+using Newtonsoft.Json;
+using PushNotification.Dominio.Entities;
+using PushNotification.Dominio.Facade;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -49,9 +55,48 @@ namespace GuiaDoApostadorWebApp.Controllers
             return con;
         }
 
+
+
         // POST: api/Concurso
-        public void Post([FromBody]string value)
+        //public void Post([FromBody]string value)
+        //{
+        //    var valores = value;
+        //}
+
+        [HttpPost]
+        public void Post(Apostador apostador)
         {
+            if (apostador == null)
+            {
+                throw new Exception("Parâmetro inválido. Apostador");
+            }
+
+            if (apostador.Apostas == null)
+            {
+                throw new Exception("Parâmetro inválido. Apostas");
+            }
+
+            for (int i = 0; i < apostador.Apostas.Count; i++)
+            {
+                Aposta aposta = apostador.Apostas[i];
+                Concurso concurso = new GuiaDoApostadorDominio.Controllers.ConcursoController(aposta.TipoConcurso).Buscar(aposta.ConcursoID);
+                
+                if (concurso != null && concurso.ID > 0)
+                {
+                    string json = JsonConvert.SerializeObject(concurso);
+
+                    PushAndroid push = new PushAndroid();
+
+                    push.RegistrationIDS.Add(apostador.RegistrationID);
+                    push.SenderID = ConfigurationManager.AppSettings["SenderIDAndroid"];
+                    push.ApplicationID = ConfigurationManager.AppSettings["ApplicationIDAndroid"];
+                    push.Title = ConfigurationManager.AppSettings["TituloPushAndroid"];
+                    push.Message = ConfigurationManager.AppSettings["MensagemPadraoPushAndroid"];
+                    push.Data = json;
+                    new PushFacade(PushNotification.Dominio.Enum.Platform.Android).Send(push);
+                    //manda o push
+                }
+            }            
         }
 
         // PUT: api/Concurso/5
